9bd9e41d883ca9fe46dbe4470d803304c40db9bc
Hyunsoo1998 <98036819+Hyunsoo1998@users.noreply.github.com>
2022-08-02 16:54:01 +0900

    java_jungsuk__
    HashSet, 예제 .

--- src/HashSetEx1.java
+++ src/HashSetEx1.java
@@ -1,22 +1,15 @@
 import java.util.*;
-
 public class HashSetEx1 {
     public static void main(String[] args) {
-        Object [] objArr = {"1", new Integer(1),"2","2","3","3","4","4","4"};
-        Set set =new HashSet();
+        Object[] objArr= {"1",new Integer(1),"2","2","3","3","4","4","4",};
+        Set set = new HashSet();
 
-        int[][] board = new int [5][5];
-        for(int i = 0; set.size() < 25; i++) {
-            set.add((int)Math.random()*50+1+"");
-        }
-        Iterator it = set.iterator();
+        for(int i =0; i<objArr.length; i++) {
+            set.add(objArr[i]); // Hashset에 objArr의 요소들을 저장한다.
 
-        for(int i = 0 ; i<board.length; i++) {
-            for(int j = 0; j<board[i].length; j++) {
-                board[i][j] = Integer.parseInt((String)it.next());
-                System.out.println((board[i][j]<10 ? " ": " ") +board[i][j]);
-            }
-            System.out.println();
         }
+        //HashSet에 저장된 요소들을 출력한다.
+        System.out.println(set);
+
     }
 }
